bd8b42b902c8e3830bdfcc3af1fa267cd21d1446
fosa12 <licznar99@gmail.com>
2022-10-18 21:45:48 +0200

    init

---
+++ PatientMovementManagement/PatientMovementManagement/Controllers/HealthChecksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatientMovementManagement.Controllers
+{
+    [Route("api/hc")]
+    [ApiController]
+    public class HealthChecksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>>GetAsync()
+        {
+            return "Healthy";
+        }
+    }
+}
